3a0a9022e00a71c05e373d34f38c8bda35758d69
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-03-15 13:53:00 +0900

    [Bronze II] Title: OX퀴즈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -3,17 +3,12 @@ import sys
 n = int(input())
 for i in range(n):
     a = list(sys.stdin.readline().rstrip())
-    grade = 0
-    j = 0
-    while j < len(a):
-        weight = 1
-        flag = True
-        while j < len(a) and a[j] == "O":
-            grade += weight
+    grade, weight = 0, 0
+    for x in a:
+        if x == "O":
             weight += 1
-            j += 1
-            flag = False
-        if flag:
-            j += 1
+            grade += weight
+        else:
+            weight = 0
 
     print(grade)
