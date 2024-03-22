6a678e0e7dcd6c4d02f6bf5e005622f586109c24
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-03-22 22:09:51 +0900

    [Gold II] Title: 철로, Time: 284 ms, Memory: 48188 KB -BaekjoonHub

---
+++ 백준/Gold/13334. 철로/철로.py
@@ -0,0 +1,27 @@
+import sys
+from heapq import *
+
+input = sys.stdin.readline
+
+n = int(input())
+lines = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    if y > x:
+        lines.append((x, y))
+    else:
+        lines.append((y, x))
+dist = int(input())
+lines.sort(key=lambda x: x[1])
+ans = 0
+h = []
+
+for st, en in lines:
+    if en - st > dist:
+        continue
+    heappush(h, st)
+    while h and en - h[0] > dist:
+        heappop(h)
+    ans = max(ans, len(h))
+ 
+print(ans)
