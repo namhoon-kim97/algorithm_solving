02359662d5116116bf733cf518c33326f6016908
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-03-24 17:40:33 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 332 ms, Memory: 47652 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,46 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def find(x):
+    if p[x] == x:
+        return x
+    return find(p[x])
+
+
+def is_diff_group(a, b):
+    a = find(a)
+    b = find(b)
+    if a == b:
+        return False
+    if p[a] == p[b]:
+        p[a] += 1
+    if p[a] < p[b]:  # b그룹이 더 큰 경우
+        p[a] = b
+    else:
+        p[b] = a
+    return True
+
+
+v, m = map(int, input().split())
+adj = []
+p = [i for i in range(v + 1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    adj.append((c, a, b))
+adj.sort()
+
+cnt = 0
+ans = 0
+for i in range(m):
+    cost, a, b = adj[i]
+    if not is_diff_group(a, b):
+        continue
+    cnt += 1
+    ans += cost
+    if cnt == v - 1:
+        break
+
+print(ans)
