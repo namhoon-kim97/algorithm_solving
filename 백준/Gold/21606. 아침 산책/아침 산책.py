27620d8bea8d6b679b5e039befdbe1e92befebe1
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-03-25 17:49:58 +0900

    [Gold III] Title: 아침 산책, Time: 1060 ms, Memory: 102080 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -5,28 +5,30 @@ sys.setrecursionlimit(10**6)
 
 
 def dfs(cur):
-    global cnt
-    if a[cur - 1] == "1" and vis[cur] != 1:
-        cnt += 1
-        return
-    vis[cur] = 0
+    vis[cur] = True
+    ret = 0
     for i in adj[cur]:
-        if vis[i] == -1:
-            dfs(i)
+        if a[i - 1] == "1":
+            ret += 1
+        elif not vis[i] and a[i - 1] == "0":
+            ret += dfs(i)
+    return ret
 
 
 n = int(input())
 a = input().strip()
 adj = [[] for _ in range(n + 1)]
+cnt = 0
 for _ in range(n - 1):
     u, v = map(int, input().split())
     adj[u].append(v)
     adj[v].append(u)
 
-cnt = 0
+    if a[u - 1] == "1" and a[v - 1] == "1":
+        cnt += 2
+vis = [False] * (n + 1)
 for i in range(1, n + 1):
-    if a[i - 1] == "1":
-        vis = [-1] * (n + 1)
-        vis[i] = 1
-        dfs(i)  # -> 여기서 i번에서 출발한 경우의 수를 구해야함.
+    if a[i - 1] == "0" and not vis[i]:
+        res = dfs(i)  # -> 여기서 i번에서 출발한 경우의 수를 구해야함.
+        cnt += res * (res - 1)
 print(cnt)
