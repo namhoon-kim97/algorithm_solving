3f78f85f0b39ed73158d44a26c72762d1fbb3814
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-03-25 16:49:00 +0900

    [Gold III] Title: 아침 산책, Time: 44 ms, Memory: 31120 KB, Score: 60 point -BaekjoonHub

---
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def dfs(cur):
+    global cnt
+    if a[cur - 1] == "1" and vis[cur] != 1:
+        cnt += 1
+        return
+    for i in adj[cur]:
+        if vis[i] == -1:
+            vis[i] = 0
+            dfs(i)
+
+
+n = int(input())
+a = input().strip()
+adj = [[] for _ in range(n + 1)]
+for _ in range(n - 1):
+    u, v = map(int, input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+
+cnt = 0
+for i in range(1, n + 1):
+    vis = [-1] * (n + 1)
+    if a[i - 1] == "1":
+        vis[i] = 1
+        dfs(i)  # -> 여기서 i번에서 출발한 경우의 수를 구해야함.
+print(cnt)
