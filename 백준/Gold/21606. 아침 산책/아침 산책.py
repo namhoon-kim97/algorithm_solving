29a7384033701111f428ecd44d571a56d1e03a9f
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-03-25 17:09:42 +0900

    [Gold III] Title: 아침 산책, Time: 1316 ms, Memory: 67772 KB, Score: 73 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,6 +1,7 @@
 import sys
 
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
 
 def dfs(cur):
@@ -8,9 +9,9 @@ def dfs(cur):
     if a[cur - 1] == "1" and vis[cur] != 1:
         cnt += 1
         return
+    vis[cur] = 0
     for i in adj[cur]:
         if vis[i] == -1:
-            vis[i] = 0
             dfs(i)
 
 
@@ -24,8 +25,8 @@ for _ in range(n - 1):
 
 cnt = 0
 for i in range(1, n + 1):
-    vis = [-1] * (n + 1)
     if a[i - 1] == "1":
+        vis = [-1] * (n + 1)
         vis[i] = 1
         dfs(i)  # -> 여기서 i번에서 출발한 경우의 수를 구해야함.
 print(cnt)
