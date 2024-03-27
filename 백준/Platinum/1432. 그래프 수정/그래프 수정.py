b5e091aaa5f441a6c173339de5f33c8a0f0c0743
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-03-27 20:47:48 +0900

    [Platinum IV] Title: 그래프 수정, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Platinum/1432. 그래프 수정/그래프 수정.py
@@ -0,0 +1,33 @@
+import sys
+from heapq import *
+
+input = sys.stdin.readline
+n = int(input())
+adj = [[] for _ in range(n + 1)]
+outdegree = [0] * (n + 1)
+res = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    conn = list(map(int, input().strip()))
+    for idx, val in enumerate(conn):
+        if val == 1:
+            adj[idx + 1].append(i)
+            outdegree[i] += 1
+
+h = []
+for i in range(1, n + 1):
+    if outdegree[i] == 0:
+        heappush(h, -i)
+while h:
+    cur = -heappop(h)
+    res[cur] = n
+    for i in adj[cur]:
+        outdegree[i] -= 1
+        if outdegree[i] == 0:
+            heappush(h, -i)
+    n -= 1
+
+if res.count(0) > 2:
+    print(-1)
+else:
+    print(" ".join(map(str, res[1:])))
