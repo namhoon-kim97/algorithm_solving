cb50540b6e5c0b5417bb8a2802cf2844c5589dea
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-03-28 11:23:34 +0900

    [Gold IV] Title: 문자열 폭발, Time: 1028 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -7,10 +7,10 @@ bomb = input().strip()
 ans = []
 for i in a:
     ans.append(i)
-    if len(ans) >= len(bomb):
-        if "".join(ans[-1 : -(len(bomb) + 1) : -1]) == bomb[::-1]:
-            for j in range(len(bomb)):
-                ans.pop()
+
+    if len(ans) >= len(bomb) and "".join(ans[len(ans) - len(bomb) :]) == bomb:
+        for j in range(len(bomb)):
+            ans.pop()
 if ans:
     print("".join(ans))
 else:
