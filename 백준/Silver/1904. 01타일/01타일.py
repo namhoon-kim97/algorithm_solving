7512caf6cb1e8195cf5fe371eb39a902e66d2cd4
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2024-03-28 20:21:59 +0900

    [Silver III] Title: 01타일, Time: 344 ms, Memory: 69652 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+d = [0] * (n + 1)
+
+
+if n > 2:
+    d[1] = 1
+    d[2] = 2
+    for i in range(3, n + 1):
+        d[i] = (d[i - 1] + d[i - 2]) % 15746
+    print(d[n])
+else:
+    print(n)
