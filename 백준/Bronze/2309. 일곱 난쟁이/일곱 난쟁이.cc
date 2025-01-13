2f5bffb01d4025533547ae7915c866929672146d
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-01-13 16:47:37 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int arr[10];
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	for (int i = 0; i < 9; i++)
+		cin >> arr[i];
+	sort(arr, arr + 9);
+	do {
+		int sum = 0;
+		for(int i = 0;  i < 7; i++) sum += arr[i];
+		if (sum == 100) break;
+	} while (next_permutation(arr, arr + 9));
+	for (int i = 0; i < 7; i++) cout << arr[i] << '\n';
+}
