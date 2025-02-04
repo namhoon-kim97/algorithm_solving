c2818c047c25c5c0fe6018119fe23e9ea0ff6af6
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-02-04 16:40:03 +0900

    [Gold IV] Title: 연구소, Time: 576 ms, Memory: 117052 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,53 +1,50 @@
 import sys
 from collections import deque
+from itertools import combinations
+import copy
 
 input = sys.stdin.readline
-
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
 
-def make_wall(cnt):
-    if cnt == 3:
-        vis = [[False] * (m + 1) for _ in range(n + 1)]
-        temp = [[0] * (m + 1) for _ in range(n + 1)]
-        q = deque()
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = a[i][j]
-                if a[i][j] == 2:
-                    q.append((i, j))
-        while q:
-            x, y = q.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                    continue
-                if vis[nx][ny] or temp[nx][ny] == 1 or temp[nx][ny] == 2:
-                    continue
-                temp[nx][ny] = 2
-                vis[nx][ny] = True
-                q.append((nx, ny))
-        ans = 0
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 0:
-                    ans += 1
-        global mx
-        mx = max(mx, ans)
-        return
-
+def spread(temp_a):
+    global res
+    q = deque()
     for i in range(n):
         for j in range(m):
-            if a[i][j] == 0:
-                a[i][j] = 1
-                make_wall(cnt + 1)
-                a[i][j] = 0
+            if temp_a[i][j] == 2:
+                q.append([i, j])
+
+    while len(q) != 0:
+        cur = q.popleft()
+        for i in range(4):
+            nx = cur[0] + dx[i]
+            ny = cur[1] + dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if temp_a[nx][ny] != 0:
+                continue
+            temp_a[nx][ny] = 2
+            q.append([nx, ny])
+
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if temp_a[i][j] == 0:
+                cnt += 1
+    res = max(res, cnt)
 
 
 n, m = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
-mx = 0
-make_wall(0)
-print(mx)
+empty_spaces = [(i, j) for i in range(n) for j in range(m) if a[i][j] == 0]
+res = 0
+
+for wall in combinations(empty_spaces, 3):
+    temp_a = copy.deepcopy(a)
+    for i, j in wall:
+        temp_a[i][j] = 1
+    spread(temp_a)
+
+print(res)
