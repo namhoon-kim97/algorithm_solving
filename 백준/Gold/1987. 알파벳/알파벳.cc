4f7fab1e1eb782722cd390ec230605c75a18b500
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-02-21 15:49:15 +0900

    [Gold IV] Title: 알파벳, Time: 460 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -8,17 +8,15 @@ int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
 int mxCnt = 0;
 int r, c;
-void dfs(int x, int y, int cnt, bitset<26>& alphabet) {
+void dfs(int x, int y, int cnt, int num) {
 	mxCnt = max(mxCnt, cnt);
 	for (int dir = 0; dir < 4; dir++) {
 		int nx = x + dx[dir];
 		int ny = y + dy[dir];
-		if (nx < 0 || ny < 0 || nx >= r || ny >= c || vis[nx][ny] || alphabet[board[nx][ny] - 'A']) continue;
-		vis[nx][ny] = true;
-		alphabet.set(board[nx][ny] - 'A');
-		dfs(nx, ny, cnt + 1, alphabet);
-		vis[nx][ny] = false;
-		alphabet.reset(board[nx][ny] - 'A');
+		if (nx < 0 || ny < 0 || nx >= r || ny >= c) continue;
+		int next = (1 << (int)(board[nx][ny] - 'A'));
+		if ((num & next) == 0) 
+			dfs(nx, ny, cnt + 1, num | next);
 	}
 }
 
@@ -36,6 +34,6 @@ int main() {
 	bitset<26> alphabet;
 	alphabet.set(board[0][0] - 'A');
 	vis[0][0] = true;
-	dfs(0, 0, 1, alphabet);
+	dfs(0, 0, 1, 1 << (int)(board[0][0] - 'A'));
 	cout << mxCnt;
 }
