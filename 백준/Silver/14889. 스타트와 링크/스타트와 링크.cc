fe45cc27c023ad899460b66b8aafde58d803353d
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-03-11 16:35:36 +0900

    [Silver I] Title: 스타트와 링크, Time: 168 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[21][21];
+bool vis[21];
+int res = INT_MAX;
+int n;
+
+void solution(int cnt, int idx) {
+	if (cnt == n / 2) {
+		int startSum = 0;
+		int linkSum = 0;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (vis[i] && vis[j]) startSum += arr[i][j];
+				else if (!(vis[i] || vis[j])) linkSum += arr[i][j];
+			}
+		}
+
+		res = min(res, abs(startSum - linkSum));
+		return;
+	}
+	for (int i = idx; i <= n; i++) {
+		if (!vis[i]) {
+			vis[i] = true;
+			solution(cnt + 1, i + 1);
+			vis[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) 
+		for (int j = 1; j <= n; j++)
+			cin >> arr[i][j];
+	
+	solution(0,1);
+	cout << res;
+}
