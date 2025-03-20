a4fd2c8228436a2805e0d507da38525fb30dd6f6
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-03-20 20:49:28 +0900

    [Gold III] Title: 줄 세우기, Time: 24 ms, Memory: 3956 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,36 +1,35 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector<int> arr[32001];
-int cnt[32001];
-vector<int> res;
+int indegree[32001];
+vector<int> adj[32001];
 int main() {
 	cin.tie(0);
-	ios_base::sync_with_stdio(0);
+	ios::sync_with_stdio(0);
 
 	int n, m;
 	cin >> n >> m;
-	
+
 	for (int i = 0; i < m; i++) {
 		int a, b;
 		cin >> a >> b;
-		arr[a].push_back(b);
-		cnt[b]++;
+		adj[a].push_back(b);
+		indegree[b]++;
 	}
-	
+
 	queue<int> q;
-	for (int i = 1; i <= n; i++) if (cnt[i] == 0) q.push(i);
+	for (int i = 1; i <= n; i++) {
+		if (indegree[i] == 0)
+			q.push(i);
+	}
 
 	while (!q.empty()) {
 		int cur = q.front(); q.pop();
-		res.push_back(cur);
-		for (int num : arr[cur]) {
-			cnt[num]--;
-			if (cnt[num] == 0) q.push(num);
+		cout << cur << ' ';
+		for (auto nxt : adj[cur]) {
+			indegree[nxt]--;
+			if (indegree[nxt] == 0)
+				q.push(nxt);
 		}
 	}
-
-	for (int ans : res) cout << ans << " ";
-
 }
