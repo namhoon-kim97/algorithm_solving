0069580e3ab9edd349a4b32e0ead7edd688e88c7
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-06-17 14:33:03 +0900

    [Silver II] Title: 격자상의 경로, Time: 108 ms, Memory: 14308 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
     static int n,m,k;
     static int[][] dp = new int[16][16];
-    static int[][] dp2 = new int[16][16];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,28 +26,29 @@ public class Main {
             System.out.println(dp[n-1][m-1]);
         }
         else{
+            k -= 1;
             int n2 = k / m;
-            int m2 = k - (m * n2) - 1;
-            dp2[0][0] = 1;
+            int m2 = k - (m * n2);
+            dp[0][0] = 1;
             for (int i = 0; i <= n2; i++){
                 for(int j = 0; j <= m2; j++){
                     if (i > 0)
-                        dp2[i][j] += dp2[i -1][j];
+                        dp[i][j] += dp[i -1][j];
                     if (j > 0)
-                        dp2[i][j] += dp2[i][j - 1];
+                        dp[i][j] += dp[i][j - 1];
                 }
             }
-            int first = dp2[n2][m2];
-            dp2[n2][m2] = 1;
+            int first = dp[n2][m2];
+            dp[n2][m2] = 1;
             for (int i = n2; i < n; i++){
                 for(int j = m2; j < m; j++){
                     if (i > n2)
-                        dp2[i][j] += dp2[i -1][j];
+                        dp[i][j] += dp[i -1][j];
                     if (j > m2)
-                        dp2[i][j] += dp2[i][j - 1];
+                        dp[i][j] += dp[i][j - 1];
                 }
             }
-            int second = dp2[n-1][m-1];
+            int second = dp[n-1][m-1];
             System.out.println(first * second);
         }
     }
