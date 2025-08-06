8e01f37e13483bce5a4a54acecbeb09c1fd9e3b3
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-08-06 14:21:43 +0900

    [Gold I] Title: 냅색문제, Time: 260 ms, Memory: 20892 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.java
+++ 백준/Gold/1450. 냅색문제/냅색문제.java
@@ -1,31 +1,29 @@
-import com.sun.source.tree.Tree;
-import java.awt.Point;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     static int n,c;
     static int[] arr = new int[31];
 
-    static void dfs(int idx, long sum, List<Long> v, int _n){
+    static void dfs(int st, int en ,int sum, List<Integer> v){
         if (sum > c) return;
-        if (idx > _n) {
+        if (st > en){
             v.add(sum);
             return;
         }
-        dfs(idx + 1, sum + arr[idx],v, _n);
-        dfs(idx + 1, sum,v, _n);
+        dfs(st + 1, en, sum + arr[st], v);
+        dfs(st + 1, en, sum, v);
     }
 
-    static int upperBound(List<Long> v, Long target){
+    static int upperB(int target, List<Integer> v){
         int st = 0;
         int en = v.size();
-        while(st < en){
+        while (st < en){
             int mid = (st + en) / 2;
-            if (v.get(mid) <= target)
-                st = mid + 1;
-            else
-                en = mid;
+            if (v.get(mid) > target) en = mid;
+            else st = mid +  1;
         }
         return st;
     }
@@ -33,22 +31,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++)
             arr[i] = Integer.parseInt(st.nextToken());
-        List<Long> v1 = new ArrayList<>();
-        List<Long> v2 = new ArrayList<>();
-        dfs(0, 0, v1, n / 2 - 1);
-        dfs(n/2, 0, v2, n-1);
+
+        List<Integer> v1 = new ArrayList<>();
+        List<Integer> v2 = new ArrayList<>();
+        dfs(0, n / 2 - 1, 0, v1);
+        dfs(n / 2, n - 1, 0, v2);
         Collections.sort(v1);
         Collections.sort(v2);
-
-        int res = 0;
-        for (Long a: v1){
-           if (c - a >= 0) res += upperBound(v2, c- a);
+        int ans = 0;
+        for (int a : v2){
+            if (c - a >= 0)
+                ans += upperB(c-a, v1);
         }
-        System.out.println(res);
+        System.out.println(ans);
     }
 }
