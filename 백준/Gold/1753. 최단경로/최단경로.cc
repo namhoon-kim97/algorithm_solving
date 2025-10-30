41777da9b53b8568f87a1791234e76290b5fd07f
namhoon-kim97 <76682500+namhoon-kim97@users.noreply.github.com>
2025-10-30 15:02:51 +0900

    [Gold IV] Title: 최단경로, Time: 92 ms, Memory: 16840 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,44 +1,39 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector<pair<int, int>> adj[20005];
-const int INF = 0x3f3f3f3f;
-int d[20005];
-int v, e, st;
+vector<pair<int,int>> adj[300001];
+int d[300001];
+const int INF = 1e9 + 10;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int v, e, k;
+	cin >> v >> e >> k;
+	fill(d, d + v + 1, INF);
+	d[k] = 0;
+	priority_queue < pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+	pq.push({ 0,k });
+	for (int i = 0; i < e; i++) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		adj[u].push_back({v,w});
+	}
 
-    cin >> v >> e >> st;
-    for (int i = 0; i < e; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        adj[a].push_back({ c,b });
-    }
+	while (!pq.empty()) {
+		auto cur = pq.top(); pq.pop();
+		if (cur.first != d[cur.second]) continue;
+		for (auto nxt : adj[cur.second]) {
+			if (d[nxt.first] <= d[cur.second] + nxt.second) continue;
+			d[nxt.first] = nxt.second + d[cur.second];
+			pq.push({ d[nxt.first], nxt.first});
+		}
+	}
 
-    fill(d, d + v + 1, INF);
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    d[st] = 0;
-    pq.push({ d[st],st});
-    while (!pq.empty()) {
-        auto cur = pq.top(); pq.pop();
-        if (d[cur.second] != cur.first) continue;
-        for (auto a : adj[cur.second]) {
-            if (d[cur.second] + a.first <= d[a.second]) {
-                d[a.second] = d[cur.second] + a.first;
-                pq.push({ d[a.second], a.second });
-            }
-        }
-    }
+	for (int i = 1; i <= v; i++) {
+		if (d[i] == INF) cout << "INF\n";
+		else cout << d[i] << '\n';
+	}
 
-    for (int i = 1; i <= v; i++) {
-        if (d[i] == INF)
-            cout << "INF" << '\n';
-        else
-            cout << d[i] << '\n';
-    }
-
-
+	return 0;
 }
